40e1d494edefc5ddc5c13926db65bb9eb9768536
Andrii Kudriashov <andrii@kudriashov.net>
2026-02-04 16:29:00 +0100

    dirty implementation

--- data_analyzer/path_publisher.py
+++ data_analyzer/path_publisher.py
@@ -4,14 +4,19 @@ import rclpy
 from rclpy.time import Time
 from rclpy.node import Node
 
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
 from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 
 
-
 class FrameListener(Node):
-    def __init__(self, base_frame='base_link', target_frame='map'):
+    def __init__(
+            self,
+            base_frame='base_link',
+            target_frame='map',
+            publish_path=False):
         super().__init__('TF_frame_listener')
         
         self.target_frame = target_frame
@@ -21,9 +26,11 @@ class FrameListener(Node):
 
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.publish_path = publish_path
+        self.path_msg = Path()
 
-        self.timer = self.create_timer(1.0, self.cb_timer)
-
+        self.timer = self.create_timer(0.1, self.cb_timer)
+        self.path_publisher = self.create_publisher(Path, 'tracked_path', 10)
     
     def cb_timer(self):
         from_frame = self.source_frame
@@ -34,7 +41,6 @@ class FrameListener(Node):
                 to_frame,
                 from_frame,
                 self.current_time,
-                timeout=rclpy.duration.Duration(seconds=1.0)
             )
         except TransformException as ex:
             self.get_logger().info(
@@ -47,7 +53,22 @@ class FrameListener(Node):
             f'Translation: x={translation.x:.2f}, y={translation.y:.2f}, z={translation.z:.2f} | '
             f'Rotation: x={rotation.x:.2f}, y={rotation.y:.2f}, z={rotation.z:.2f}, w={rotation.w:.2f}'
         )
+        self.update_path(tf_data)
+        if self.publish_path:
+            self.path_publisher.publish(self.path_msg)
 
+    def update_path(self, tf_data):
+        pose = PoseStamped()
+        pose.header = tf_data.header
+        pose.pose.position.x = tf_data.transform.translation.x
+        pose.pose.position.y = tf_data.transform.translation.y
+        pose.pose.position.z = tf_data.transform.translation.z
+        pose.pose.orientation = tf_data.transform.rotation
+
+        self.path_msg.header = tf_data.header
+        self.path_msg.poses.append(pose)
+
+        self.get_logger().info(f'Path length: {len(self.path_msg.poses)}')
 
 
 def main():
@@ -70,18 +91,24 @@ def main():
         default='map',
         required=False
     )
+    parser.add_argument(
+        "--publish_path",
+        action='store_true',
+        help="Publish the path based on the transforms"
+    )
     args = parser.parse_args()
 
 
     print("Starting frame listener node...")
     rclpy.init()
-    node = FrameListener(args.base_frame, args.target_frame)
+    node = FrameListener(args.base_frame, args.target_frame, args.publish_path)
     node.get_logger().info('Starting frame listener node')
+    if args.publish_path:
+        node.get_logger().info('Path publishing enabled')
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
         pass
-
     rclpy.shutdown()
 
 
