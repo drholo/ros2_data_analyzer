5c48e6a3f34333f7dacd29c090635a6ac074e373
Andrii Kudriashov <andrii@kudriashov.net>
2026-02-06 13:46:43 +0100

    Fix plotting and Odom subscription

--- data_analyzer/plotter.py
+++ data_analyzer/plotter.py
@@ -16,16 +16,18 @@ def plot_2d_traj(subscribers, interval=100):
         ax.set_prop_cycle(cycler("color", colors))
         ax.set_aspect("equal", "box")
         ax.grid(True, linestyle="-.", alpha=0.3)
+        _mx = []
+        _my = []
 
         for subscriber in subscribers:
             x, y = subscriber.get_trajectory_data()
             if len(x) > 0 and len(y) > 0:
                 ax.plot(x, y, label=subscriber.model.node_name, alpha=0.8)
-                max_val = max(max(x), max(y))
-                min_val = min(min(x), min(y))
-            else:
-                min_val = 0
-                max_val = 0
+                _mx.append(max(max(x), max(y)))
+                _my.append(min(min(x), min(y)))
+
+        max_val = max(_mx) if _mx else 0
+        min_val = min(_my) if _my else 0
 
         ax.set_xlabel("x [m]", fontsize=12)
         ax.set_ylabel("y [m]", fontsize=12)
